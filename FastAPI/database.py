54219e84fb8a589d8f8b492277921a4d1ebde5e7
leon <90852553+leon0719@users.noreply.github.com>
2023-10-31 16:05:22 +0800

    📦 NEW: init

---
+++ FastAPI/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from config import settings
+
+
+URL_DATABASE = settings.database_url
+
+engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ FastAPI/main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="FastAPI-React-APP", summary="使用 React 作為前端，FastAPI 作為後端，SQLite 作為我們的資料庫"
+)
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class TransactionBase(BaseModel):
+    amount: float
+    category: str
+    description: str
+    is_income: bool
+    date: str
+
+
+class TransactionModel(TransactionBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
+
+
+@app.get("/", tags=["root"], description="測試是否成功開啟")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/transactions", tags=["transactions"], response_model=TransactionModel)
+async def create_transaction(transaction: TransactionBase, db: db_dependency):
+    db_transaction = models.Transaction(**transaction.model_dump())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+
+@app.get("/transactions", tags=["transactions"], response_model=list[TransactionModel])
+async def get_transactions(db: db_dependency):
+    transactions = db.query(models.Transaction).all()
+    return transactions
